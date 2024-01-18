43773b4739ff09ca313bba7f87f7a977dd654ac7
ishans619 <ishanshukla619@gmail.com>
2024-01-18 23:17:56 +0530

    small change in the Node class

--- Node.java
+++ Node.java
@@ -2,10 +2,11 @@ public class Node {
     int data;
     Node next;
 
-    public Node(int data){
+    public Node(int data){// constructor to initialize the object of the Node class 
         this.data = data;
         this.next = null;
 
     }
     
 };
+    
