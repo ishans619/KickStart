10928ad01325ebb31b90a9f08f051cc3a4f73bb2
ishans619 <ishanshukla619@gmail.com>
2024-01-30 19:18:42 +0530

    coverting given array to linked list by traversing and printing the elements

---
+++ ConvertingArrToLL.java
@@ -0,0 +1,34 @@
+class Node{
+    int data;
+    Node next;
+
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class ConvertingArrToLL {
+    private static Node ConvertToLL(int [] arr){
+        Node head = new Node(arr[0]);
+        Node mover = head;
+        for(int i = 1; i < arr.length; i++){
+            Node temp = new Node(arr[i]);
+            mover.next = temp;
+            mover = temp;
+        }
+        return head;
+    }
+
+    public static void main(String [] args){
+        int arr[] = {1,2,3,4,5,6,7};
+        Node head = ConvertToLL(arr);
+        
+        Node temp = head;
+        while(temp != null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+    
+}
